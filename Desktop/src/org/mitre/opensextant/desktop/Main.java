7785314c86c3137d8ac5fae0fd6c896c80b04e32
Andre Quina <andre.quina@gmail.com>
2013-05-16 10:59:27 -0400

    restructured classes and broke some things up.  Also added a open sextant home selection frame

---
+++ Desktop/src/org/mitre/opensextant/desktop/Main.java
@@ -0,0 +1,111 @@
+package org.mitre.opensextant.desktop;
+
+import java.awt.Color;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.swing.UIManager;
+
+import org.mitre.opensextant.desktop.ui.OpenSextantMainFrameImpl;
+import org.mitre.opensextant.desktop.ui.SelectOSHomeFrameImpl;
+import org.mitre.opensextant.desktop.ui.forms.ConfigFrame;
+import org.mitre.opensextant.desktop.util.Initialize;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+	      ConfigFrame.loadConfig();
+	      log.info("loaded config");
+	      
+	      Properties props = System.getProperties();
+	      
+	      String osHome = ConfigFrame.getOsHome();
+	      
+	      if (osHome == null || osHome.trim().length() == 0) {
+		      osHome = props.getProperty("opensextant.home");
+		      if(osHome == null) {
+		    	  
+		    	  List<String> osHomes = new ArrayList<String>() {{
+		    		  add("opensextant");
+		    		  add((new File("")).getAbsolutePath()+File.separator+"opensextant");
+		    		  add((new File("")).getAbsolutePath()+File.separator+"dist"+File.separator+"opensextant");
+		    	  }};
+		    	  
+		    	  for (String potentialHome : osHomes) {
+		        	  if ((new File(potentialHome)).exists()) {
+		        		  osHome = potentialHome;
+		        		  log.info("Open sextant home set to: " + osHome);
+		        		  break;
+		        	  }
+		    	  }
+		    	  
+		      }
+
+		      if(osHome == null || !(new File(osHome)).exists()) { 
+		    	  new SelectOSHomeFrameImpl();
+		      } 
+	      }
+	      
+		  props.setProperty("opensextant.home", osHome); 
+	      ConfigFrame.setOsHome(osHome);
+	      ConfigFrame.saveSettings();
+          Initialize.init();
+
+    	  openMainWindow();
+
+	}
+	
+	public static void openMainWindow() {
+        log.info("Starting Desktop Client");
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+             UIManager.setLookAndFeel(
+               UIManager.getSystemLookAndFeelClassName());
+                  if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    UIManager.getLookAndFeelDefaults().put("nimbusOrange", (Color.green));
+                    break;
+                  }
+            }
+        } catch (ClassNotFoundException ex) {
+            log.error(ex.getMessage()); 
+        } catch (InstantiationException ex) {
+            log.error(ex.getMessage()); 
+        } catch (IllegalAccessException ex) {
+            log.error(ex.getMessage());  
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            log.error(ex.getMessage()); 
+        }
+        //</editor-fold>
+//        Initialize.init();
+//        Properties props = System.getProperties();
+//        if(props.getProperty("opensextant.home") == null)
+//          props.setProperty( "opensextant.home"
+//                           , ApiHelper.BASE_PATH + "opensextant"); 
+      /*  try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(OpenSextant.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(OpenSextant.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(OpenSextant.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(OpenSextant.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new OpenSextantMainFrameImpl().setVisible(true);
+            }
+        });
+
+	}
+	
+}
---
+++ Desktop/src/org/mitre/opensextant/desktop/util/Initialize.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mitre.opensextant.desktop.util;
+
+import org.mitre.opensextant.apps.OpenSextantRunner;
+
+/**
+ *
+ * @author GBLACK
+ */
+public class Initialize implements Runnable {
+  private static boolean isInitialized = false;
+  private static boolean startedInit = false;
+  
+  public static boolean getInitialized() { return isInitialized; }
+
+  public void run() {
+    try{ (new OpenSextantRunner()).initialize(); }
+    catch (Exception e) { e.printStackTrace(); } 
+      
+    Initialize.isInitialized = true;
+  }
+  
+  public static void init(){
+    if(startedInit) return;
+    startedInit = true;
+ 
+    (new Thread(new Initialize())).start();
+  }
+
+}
