e7c8af0b5c34a445d08228dfd126be017eac889d
Gavin Black <gblack@mitre.org>
2013-06-13 10:43:58 -0400

    Removed debug statement

--- Desktop/src/org/mitre/opensextant/desktop/ui/forms/panels/RowDurationImpl.java
+++ Desktop/src/org/mitre/opensextant/desktop/ui/forms/panels/RowDurationImpl.java
@@ -63,7 +63,6 @@ public class RowDurationImpl extends RowDuration {
 				if (minutes > 0) durationString += minutes + " min ";
 				
 				durationString += seconds + " sec";
-				System.out.println("PROGRESS:" + row.getPercent());
 				if (estimatedDuration > 0 && updatedDuration > 10*1000 && row.getPercent() > 1) {
 					long estMinutes = TimeUnit.MILLISECONDS.toMinutes(estimatedDuration);
 					long estSeconds = TimeUnit.MILLISECONDS.toSeconds(estimatedDuration) - TimeUnit.MINUTES.toSeconds(estMinutes);
