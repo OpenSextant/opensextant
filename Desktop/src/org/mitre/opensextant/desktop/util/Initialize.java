bbdc530d82c5d590f9d52f64a0399351e8f96413
Andre Quina <andre.quina@gmail.com>
2013-06-24 12:01:49 -0400

    added pump priming to initialization that runs OpenSextant against a list of world and us cities to get Solr and Gate files loaded into OS cache

--- Desktop/src/org/mitre/opensextant/desktop/util/Initialize.java
+++ Desktop/src/org/mitre/opensextant/desktop/util/Initialize.java
@@ -4,10 +4,22 @@
  */
 package org.mitre.opensextant.desktop.util;
 
+import gate.Document;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.mitre.opensextant.apps.Config;
 import org.mitre.opensextant.apps.OpenSextantRunner;
+import org.mitre.opensextant.desktop.executor.opensextant.ext.converter.XTextConverter;
+import org.mitre.opensextant.desktop.executor.opensextant.ext.geocode.OSGeoCoder;
+import org.mitre.opensextant.desktop.ui.helpers.ConfigHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +28,8 @@ import org.slf4j.LoggerFactory;
  * @author GBLACK
  */
 public class Initialize implements Runnable {
-	private static boolean isInitialized = false;
+	private static final int PRIME_COUNT = 2;
+    private static boolean isInitialized = false;
 	private static boolean startedInit = false;
 
 	public static boolean getInitialized() {
@@ -26,22 +39,47 @@ public class Initialize implements Runnable {
 	private static Logger log = LoggerFactory.getLogger(Initialize.class);
 
 	public void run() {
-		try {
+	    FileWriter writer = null;
+	    InputStream reader = null;
+	    try {
 			log.info("Running initialization");
 			log.info("Gate home: " + Config.GATE_HOME);
 			log.info("Solr home: " + Config.SOLR_HOME);
-			(new OpenSextantRunner()).initialize();
-			log.info("runner initialized");
+            XTextConverter converter = new XTextConverter(ConfigHelper.getInstance().getXTextCacheRoot());
+            OSGeoCoder geoCoder = new OSGeoCoder();
+            geoCoder.initialize();
+            log.info("runner initialized");
+            log.info("Priming the pump by running data");
+            
+            File primer = File.createTempFile("world_and_us_cities", ".txt");
+            primer.deleteOnExit();
+            writer = new FileWriter(primer);
+            reader = getClass().getResourceAsStream("/world_and_us_cities.txt");
+            IOUtils.copy(reader, writer);
+            log.info(primer.getAbsolutePath());
+            List<Document> documents = converter.convert(primer);
+            for (int index = 0; index < PRIME_COUNT; index++) {
+                for (Document document : documents) {
+                    geoCoder.geoCodeText(document);
+                }
+            }
+            log.info("Done priming.");
+            
+            
 		} catch (Throwable e) {
 			log.error("error initializing: ", e);
 			JOptionPane.showMessageDialog(null, "Error initializing, " + e.getMessage(), "Error Initializing", JOptionPane.ERROR_MESSAGE);
 			e.printStackTrace();
+		} finally {
+            IOUtils.closeQuietly(reader);
+            IOUtils.closeQuietly(writer);
 		}
+	    
 
 		Initialize.isInitialized = true;
 	}
 
-	public static void init() {
+	public static synchronized void init() {
 		log.info("initializing...");
 		if (startedInit)
 			return;
