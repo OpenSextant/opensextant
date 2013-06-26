96daac0b567d8ed1ecc755eb0ed4f6bd8b071756
Ryan Warner <rtwarner@mitre.org>
2013-06-26 15:13:00 -0400

    Statistics added and fixed for informational purposes

--- Desktop/src/org/mitre/opensextant/desktop/util/JobStatistics.java
+++ Desktop/src/org/mitre/opensextant/desktop/util/JobStatistics.java
@@ -5,6 +5,8 @@
 package org.mitre.opensextant.desktop.util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 import org.mitre.opensextant.processing.Geocoding;
 
@@ -18,20 +20,36 @@ public class JobStatistics {
     public static final int COUNTRY = 2;
     
     private int objCount = 0;
-    private ArrayList<Geocoding>[] geos = new ArrayList[COUNTRY + 1];
+    private ArrayList<String>[] geos = new ArrayList[COUNTRY + 1];
+    private HashMap[] geoHash = new HashMap[COUNTRY + 1];
     
     public void addGeo(Geocoding g, int arrayLoc) {
-        if(geos[arrayLoc] == null) geos[arrayLoc] = new ArrayList<Geocoding>();
-        geos[arrayLoc].add(g);
+        String s = g.toString();
+        if(s == null) return;
+        int posCoord = s.indexOf(" @(");
+        if(posCoord >= 0) s = s.substring(0, posCoord);
+        if(geoHash[arrayLoc] == null) geoHash[arrayLoc] = new HashMap();
+        
+        if(geoHash[arrayLoc].containsKey(s))
+        {
+            int count = (int)geoHash[arrayLoc].get(s);
+            geoHash[arrayLoc].put(s, ++count);
+        }
+        else
+            geoHash[arrayLoc].put(s, 1);
     }
     
-    public ArrayList<Geocoding> getGeo(int arrayLoc) {
-        return geos[arrayLoc];
+    public Set<String> getGeo(int arrayLoc) {
+        return geoHash[arrayLoc].keySet();
+    }
+    
+    public int getCount(int arrayLoc, String val){
+        return (int)geoHash[arrayLoc].get(val);
     }
     
     public int getCount(int arrayLoc){
-        if(geos[arrayLoc] == null) return 0;
-        return geos[arrayLoc].size();
+        if(geoHash[arrayLoc] == null) return 0;
+        return geoHash[arrayLoc].size();
     }
     
     public void incrementObjCount() {
